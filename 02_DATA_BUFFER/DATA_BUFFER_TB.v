c636754028f04f8df6d51be7b40b038cffa6c359
AUDIY <96096729+AUDIY@users.noreply.github.com>
2023-12-23 19:07:03 +0900

    Update DATA_BUFFER_TB.v
    Change License CERN-OHL-S v2 to CERN-OHL-W v2.

--- 02_DATA_BUFFER/DATA_BUFFER_TB.v
+++ 02_DATA_BUFFER/DATA_BUFFER_TB.v
@@ -3,29 +3,28 @@
 *
 * Test bench for DATA_BUFFER.v
 *
-* Version: 0.11
+* Version: 0.12
 * Author : AUDIY
-* Date   : 2023/12/03
+* Date   : 2023/12/23
 *
 * License
 --------------------------------------------------------------------------------
 | Copyright AUDIY 2023.                                                        |
 |                                                                              |
-| This source describes Open Hardware and is licensed under the CERN-OHL-S v2. |
+| This source describes Open Hardware and is licensed under the CERN-OHL-W v2. |
 |                                                                              |
 | You may redistribute and modify this source and make products using it under |
-| the terms of the CERN-OHL-S v2 (https://ohwr.org/cern_ohl_s_v2.txt).         |
+| the terms of the CERN-OHL-W v2 (https:/cern.ch/cern-ohl).                    |
 |                                                                              |
 | This source is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,          |
 | INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A         |
-| PARTICULAR PURPOSE. Please see the CERN-OHL-S v2 for applicable conditions.  |
+| PARTICULAR PURPOSE. Please see the CERN-OHL-W v2 for applicable conditions.  |
 |                                                                              |
 | Source location: https://github.com/AUDIY/FIR_x2                             |
 |                                                                              |
-| As per CERN-OHL-S v2 section 4, should You produce hardware based on this    |
-| source, You must where practicable maintain the Source Location visible      |
-| on the external case of the Gizmo or other products you make using this      |
-| source.                                                                      |
+| As per CERN-OHL-W v2 section 4.1, should You produce hardware based on these |
+| sources, You must maintain the Source Location visible on the external case  |
+| of the FIR_x2 or other products you make using this source.                  |
 --------------------------------------------------------------------------------
 *
 -----------------------------------------------------------------------------*/
