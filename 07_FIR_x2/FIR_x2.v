d94b2ec5c8bf58473efbfb262c1b5dfdd20ad80e
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-01-19 14:26:55 +0900

    Update FIR_x2.v
    Change version to 1.00

--- 07_FIR_x2/FIR_x2.v
+++ 07_FIR_x2/FIR_x2.v
@@ -3,9 +3,9 @@
 *
 * Oversampling FIR Filter Module (Oversampling Ratio: x2)
 *
-* Version: 0.16
+* Version: 1.00
 * Author : AUDIY
-* Date   : 2023/12/23
+* Date   : 2025/01/20
 *
 * Port
 *   Input
