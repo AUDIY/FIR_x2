cebc844f1bcb8a70d743fd54bfed7104621c1ca1
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-06-21 14:29:02 +0900

    Add Questa Simulation environment

---
+++ 06_ADD/Questa/ADD.bat
@@ -0,0 +1,15 @@
+vlib work
+
+vmap work work
+
+vlog +cover=bcs ../ADD.v
+vlog ../../01_DPRAM_CONT/DPRAM_CONT.v
+vlog ../../02_DATA_BUFFER/SDPRAM_SINGLECLK.v
+vlog ../../02_DATA_BUFFER/DATA_BUFFER.v
+vlog ../../03_SPROM_CONT/SPROM_CONT.v
+vlog ../../04_FIR_COEF/SPROM.v
+vlog ../../04_FIR_COEF/FIR_COEF.v
+vlog ../../05_MULT/MULT.v
+vlog ../ADD_TB.v
+
+vsim -debugdb=+acc work.ADD_TB -voptargs=+acc -coverage -do "do run.do"
